8e394431f046ebee1bd5d0592498c2e55ea2c5bb
Andrii <latasandrej@gmail.com>
2021-12-15 22:28:15 +0200

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int[] randomArr(int length, int minValue = -10, int maxValue = 10)  
+        {
+            int[] array = new int[length];
+        Random random = new Random();
+           for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue);
+
+            }
+            return array;
+        }
+
+
+        public static int task1(int [] array)
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] < 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+        public static int task2(int[] array)
+        {
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] % 2 == 0)
+                {
+                   if(array[i] > max)
+                    {
+                        max = array[i];
+                    }
+                }
+            }
+            return max;
+        }
+        public static int task3(int[] array)
+        {
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+        public static int task4(int[] array)
+        {
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                
+                    if (array[i] > max)
+                    {
+                        max = i;
+                    }
+                
+            }
+            return max;
+        }
+        public static int task5(int[] array)
+        {
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+               
+                    if (Math.Abs(array[i]) > max)
+                    {
+                        max = array[i];
+                    }
+                
+            }
+            return max;
+        }
+        public static int task6(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+
+
+                if (array[i] > 0)
+                {
+                    sum += i;
+                }
+
+            }
+            return sum;
+        }
+        public static int task7(int[] array)
+        {
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+               if(array[i] % 2 != 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+    }
+    
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Знайти :\n1 суму від’ємних елементів масиву  \n2 максимальний серед парних елементів масиву; \n3 добуток елементів масиву, що мають парні індекси; \n4 номер (індекс) максимального елемента масиву;\n5 максимальний за модулем елемент масиву; \n6 суму індексів додатних елементів;\n7 кількість непарних елементів масиву.");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Количество елементов = ");
+            int n = int.Parse(Console.ReadLine());
+            int [] arr = ClassLibrary1.Class1.randomArr(n);
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]} ");
+
+            }
+
+            switch (b)
+            {
+                case 1:
+                    int max = ClassLibrary1.Class1.task1(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 2:
+                     max = ClassLibrary1.Class1.task2(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 3:
+                     max = ClassLibrary1.Class1.task3(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 4:
+                     max = ClassLibrary1.Class1.task4(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 5:
+                     max = ClassLibrary1.Class1.task5(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 6:
+                     max = ClassLibrary1.Class1.task6(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+                case 7:
+                     max = ClassLibrary1.Class1.task7(arr);
+                    Console.WriteLine($"sum = {max}");
+                    break;
+            }
+            
+
+        }
+    }
+}
